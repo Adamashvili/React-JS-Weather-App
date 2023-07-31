8f546eb93a519e683b67eb3ccbccf46c4d1e4a4d
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-07-31 23:38:56 +0400

    add search bar

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ad6a676033msh549fd474911f80bp1b062fjsn3feaf50ecab9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
